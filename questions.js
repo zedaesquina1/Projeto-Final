7b4870d710e631839ce3920f0e880d52c0fe2e9b
Daniel <116726675+zedaesquina1@users.noreply.github.com>
2023-08-08 01:03:48 -0300

    novas atualizações

---
+++ questions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        question: "qual é a capital da frança",
+        answers: [
+            { option: "paris", correct: true },
+            { option: "sei la", correct: false },
+            { option: "fenda do biquini", correct: false },
+          ],
+    },
+    {
+        question: "qual é a capital da frança",
+        answers: [
+            { option: "paris", correct: true },
+            { option: "sei la", correct: false },
+            { option: "fenda do biquini", correct: false },
+          ],
+    },
+    {
+        question: "qual é a capital da frança",
+        answers: [
+            { option: "paris", correct: true },
+            { option: "sei la", correct: false },
+            { option: "fenda do biquini", correct: false },
+          ],
+    },
+    {
+        question: "qual é a capital da frança",
+        answers: [
+            { option: "paris", correct: true },
+            { option: "sei la", correct: false },
+            { option: "fenda do biquini", correct: false },
+          ],
+    }
+]
