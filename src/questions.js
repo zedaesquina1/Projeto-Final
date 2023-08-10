997e64468bad60b2f84aac3facb202494226c904
Daniel <116726675+zedaesquina1@users.noreply.github.com>
2023-08-10 14:21:25 -0300

    inicio

---
+++ src/questions.js
@@ -0,0 +1,152 @@
+export default [
+    {
+        question: "Que cidade brasileira é conhecida por chover todos os dias quase à mesma hora?",
+        answers: [
+            { option: "São Paulo", correct: false },
+            { option: "Belo Horizonte", correct: false },
+            { option: "Natal", correct: false },
+            { option: "Rio Grando do Sul", correct: false },
+            { option: "Belém", correct: true },
+          ],
+    },
+    {
+        question: "Qual é a capital do Brasil",
+        answers: [
+            { option: "Brasília", correct: true },
+            { option: "Acre", correct: false },
+            { option: "Rio De Janeiro", correct: false },
+            { option: "São Paulo", correct: true },
+            { option: "Bahia", correct: false },
+          ],
+    },
+    {
+        question: "Qual é o maior rio do Brasil?",
+        answers: [
+            { option: "Rio Purus", correct: false },
+            { option: "Rio Paraguai", correct: false },
+            { option: "Rio Tietê", correct: false },
+            { option: "Rio Negro", correct: false },
+            { option: "Rio Amazonas", correct: true },
+          ],
+    },
+    {
+        question: "Qual é o ponto mais alto do Brasil?",
+        answers: [
+            { option: "Pico da Neblina", correct: true },
+            { option: "Pico 31 de março", correct: false },
+            { option: "Monte Roraima", correct: false },
+            { option: "Pico das Agulhas Negras", correct: false },
+            { option: "Pico do Calçado", correct: false },
+          ],
+    },
+    {
+        question: "Que grande evento histórico aconteceu em 1822 no Brasil?",
+        answers: [
+            { option: "Descobrimento do Brasil", correct: false },
+            { option: "Independência do Brasil", correct: true },
+            { option: "Construção de Brasilia", correct: false },
+            { option: "Revolução de 1930", correct: false },
+            { option: "Ditadura Militar", correct: false },
+          ],
+    },
+    {
+        question: "O que comemora no feriado do dia 21 de abril no Brasil?",
+        answers: [
+            { option: "Natal", correct: false },
+            { option: "Tiradentes", correct: true },
+            { option: "Finados", correct: false },
+            { option: "Nossa Senhora Aparecida", correct: false },
+            { option: "Independência do Brasil", correct: false },
+          ],
+    },
+    {
+        question: "Qual o monumento mais famoso do brasil?",
+        answers: [
+            { option: "Catedral da Sé", correct: false },
+            { option: "Os Candangos", correct: false },
+            { option: "Elevador Lacerda", correct: false },
+            { option: "Cristo Redentor", correct: true },
+            { option: "Ópera de Arame", correct: false },
+          ],
+    },
+    {
+        question: "Em que região se localiza o estado de Minas Gerais?",
+        answers: [
+            { option: "Sudeste", correct: true },
+            { option: "Centro-Oeste", correct: false },
+            { option: "Norte", correct: false },
+            { option: "Sul", correct: false },
+            { option: "Nordeste", correct: false },
+          ],
+    },
+    {
+        question: "Quem foi o primeiro presidente do Brasil?",
+        answers: [
+            { option: "Luiz Inácio Lula da Silva", correct: false },
+            { option: "Floriano Peixoto", correct: false },
+            { option: "Marechal Deodoro da Fonseca", correct: true },
+            { option: "Jair Bolsonaro", correct: false },
+            { option: "Michel Temer", correct: false },
+          ],
+    },
+    {
+        question: "Qual é o bioma predominante na região central do Brasil?",
+        answers: [
+            { option: "Caatinga", correct: false },
+            { option: "Mata Atlântica", correct: false },
+            { option: "Pampa", correct: false },
+            { option: "Pantanal", correct: false },
+            { option: "Cerrado", correct: true },
+          ],
+    },
+    {
+        question: "Quem proclamou a independência do Brasil?",
+        answers: [
+            { option: "Dom Pedro I", correct: true },
+            { option: "Maria Joaquina de Assis", correct: false },
+            { option: "Pedro Alvares Cabrau", correct: false },
+            { option: "Dom Pedro III", correct: false },
+            { option: "Dilma Roussef ", correct: false },
+          ],
+    },
+    {
+        question: "Quem é considerado o fundador da cidade do Rio de Janeiro?",
+        answers: [
+            { option: "Martin Afonso de Souza", correct: false },
+            { option: "Tomé de Souza", correct: false },
+            { option: "Estacio de Sá", correct: true },
+            { option: "Fernandes Furtado de Mendonça", correct: false },
+            { option: "Martim Afonso de Sousa", correct: false },
+          ],
+    },
+    {
+        question: "Qual é o esporte mais popular no Brasil?",
+        answers: [
+            { option: "Corrida", correct: false },
+            { option: "Skate", correct: false },
+            { option: "Vôlei", correct: false },
+            { option: "Futebol", correct: true },
+            { option: "Ciclismo", correct: false },
+          ],
+    },
+    {
+        question: "Qual é o evento de Carnaval mais famoso do Brasil?",
+        answers: [
+            { option: "O Carnaval do Rio de Janeiro", correct: true },
+            { option: "O Carnaval do Amazonas", correct: false },
+            { option: "O Carnaval de São Paulo", correct: false },
+            { option: "O Carnaval da Bahia", correct: false },
+            { option: "O Carnaval do Rio Grande do Sul", correct: false },
+          ],
+    },
+    {
+        question: "Qual é o continente onde se localiza o Brasil?",
+        answers: [
+            { option: "América", correct: true },
+            { option: "África", correct: false },
+            { option: "Ásia", correct: false },
+            { option: "Europa", correct: false },
+            { option: "Oceania", correct: false },
+          ],
+    },
+]
